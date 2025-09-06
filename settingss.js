8b1cdd5e06938ceb216d71a11d0705fae20efb3f
Daricko <derickmndeme668@gmail.com>
2025-09-06 17:55:46 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~EmEWgA4K#6DMPd-jj2HP7JJVZcRNDaNnRSgZQB6m_19oMDk9m1BM",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2557501134", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
